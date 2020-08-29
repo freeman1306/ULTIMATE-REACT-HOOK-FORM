92a9c2fd12fb59de918998104d2827f30d1fa684
freeman1306 <zavalkin06@gmail.com>
2020-08-29 13:45:03 +0300

    form is done

--- src/Step1.jsx
+++ src/Step1.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
+import { MainContainer } from './components/MainContainer';
+import { Form } from './components/Form';
+import { Input } from './components/Input';
+import { PrimaryButton } from './components/PrimaryButton';
+import Typography from '@material-ui/core/Typography';
+import { yupResolver } from '@hookform/resolvers';
+import * as yup from 'yup';
 
-export function Step1() {
-  const { register, handleSubmit, erros } = useForm();
+const schema = yup.object().shape({
+  firstName: yup
+    .string()
+    .matches(/^([^0-9]*)$/, 'First name should not contain numbers')
+    .required('First name is a required field'),
+  lastName: yup
+    .string()
+    .matches(/^([^0-9]*)$/, 'Last name should not contain numbers')
+    .required('Last name is a required field'),
+});
+
+export const Step1 = () => {
+  const { register, handleSubmit, errors } = useForm({
+    mode: 'onBlur',
+    resolver: yupResolver(schema),
+  });
   const history = useHistory();
 
   const onSubmit = (data) => {
@@ -11,13 +32,30 @@ export function Step1() {
   };
 
   return (
-    <div>
-      <h2>Step 2</h2>
-      <form>
-        <input ref={register} name="firstName" type="text" placeholder="First Name" />
-        <input ref={register} name="firstName" type="text" placeholder="First Name" />
-        <button type="submit">Next</button>
-      </form>
-    </div>
+    <MainContainer>
+      <Typography component="h2" variant="h5">
+        Step 1
+      </Typography>
+
+      <Form>
+        <Input
+          ref={register}
+          name="firstName"
+          type="text"
+          label="First Name"
+          error={!!errors.firstName}
+          helperText={errors?.firstName?.message}
+        />
+        <Input
+          ref={register}
+          name="lastName"
+          type="text"
+          label="Last Name"
+          error={!!errors.lastName}
+          helperText={errors?.lastName?.message}
+        />
+        <PrimaryButton>Next</PrimaryButton>
+      </Form>
+    </MainContainer>
   );
-}
+};
---
+++ src/components/MainContainer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Container from '@material-ui/core/Container'
+import { makeStyles } from '@material-ui/core/styles'
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        marginTop: theme.spacing(4),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+    }
+}))
+
+export function MainContainer({ children, ...props }) {
+    const styles = useStyles()
+
+    return (
+        <Container className={styles.root} component='main' maxWidth='xs' {...props}>
+            {
+                children
+            }
+       </Container>
+    )
+}
